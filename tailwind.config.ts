51951f0ba58439960280a7623ab62033ec3d70f3
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-01-08 11:03:11 -0300

    chore: add customised colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,15 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      'white': '#FFFFFF',
+      'black': '#000000',
+      'gray': {
+        100: '#F5F6F8',
+        200: '#DFDFE0',
+        600: '#333333',
       },
+      'primary': '#2F5DF1'
     },
   },
   plugins: [],
