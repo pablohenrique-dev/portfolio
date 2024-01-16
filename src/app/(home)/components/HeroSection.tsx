e3f0203b7a3c52891f8b510ba84b4086a57e2f17
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-01-16 14:47:29 -0300

    feat: add CMS infos to HeroSection

--- src/app/(home)/components/HeroSection.tsx
+++ src/app/(home)/components/HeroSection.tsx
@@ -1,7 +1,12 @@
+import { HomePageInfo } from "@/@types/pageInfo";
 import { AnchorLink } from "@/components/AnchorLink";
 import { Image } from "@/components/Image";
 
-export const HeroSection = () => {
+type HeroSection = {
+  homeInfos: HomePageInfo;
+};
+
+export const HeroSection = ({ homeInfos }: HeroSection) => {
   return (
     <main
       id="top"
@@ -10,9 +15,7 @@ export const HeroSection = () => {
       <div className="container flex flex-col items-center justify-between gap-8 pb-12 pt-10 sm:pb-[8rem] sm:pt-[6rem] lg:flex-row lg:gap-16">
         <div>
           <h1 className="mb-4 block text-[40px] font-semibold text-white sm:text-5xl sm:leading-tight md:text-[5rem]">
-            Desenvolvedor
-            <br />
-            Front-end
+            {homeInfos.title}
             <Image
               src="/assets/icon-code-detail.svg"
               alt="Imagem com um quadrado azul com um ícone representando a programação"
@@ -20,8 +23,7 @@ export const HeroSection = () => {
             />
           </h1>
           <p className="mb-8 text-lg font-light text-white opacity-80 sm:mb-10 sm:text-2xl sm:leading-normal">
-            Construindo experiências digitais por meio da tecnologia,
-            criatividade e design.
+            {homeInfos.introduction}
           </p>
 
           <div className="flex items-start gap-6 max-[560px]:flex-col">
@@ -39,7 +41,7 @@ export const HeroSection = () => {
           </div>
         </div>
         <Image
-          src="/assets/profile-photo.jpg"
+          src={homeInfos.profilePicture.url}
           alt="Selfie de jovem sentado em uma cadeira e sorrindo"
           className="w-full md:min-w-[22rem] lg:w-[27rem]"
         />
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,64 @@
+import { fetchHygraphQuery } from "@/utils/fetchHygraphQuery";
 import { About } from "./components/About/About";
 import { CertificatesSection } from "./components/Certificate/CertificatesSection";
 import { HeroSection } from "./components/HeroSection";
 import { ProjectsSection } from "./components/Project/ProjectsSection";
+import { HomePageData } from "@/@types/pageInfo";
 
-export default function Home() {
+async function getPageData(): Promise<HomePageData> {
+  const query = `
+    query MyQuery {
+      page(where: {slug: "home"}) {
+        introduction
+        title
+        profilePicture {
+          url
+        }
+        highlightProjects {
+          githubUrl
+          liveProjectUrl
+          description {
+            raw
+          }
+          slug
+          technologies {
+            name
+          }
+          thumbnail {
+            url
+          }
+          title
+          pageThumbnail {
+            url
+          }
+        }
+        highlightCertificates {
+          title
+          iconSvg
+          certificateUrl
+          cardColor {
+            rgba {
+              r
+              g
+              b
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  return fetchHygraphQuery(query);
+}
+
+export default async function Home() {
+  const { page: pageInfos } = await getPageData();
+
   return (
     <>
-      <HeroSection />
-      <ProjectsSection />
-      <CertificatesSection />
+      <HeroSection homeInfos={pageInfos} />
+      <ProjectsSection projects={pageInfos.highlightProjects} />
+      <CertificatesSection certificates={pageInfos.highlightCertificates} />
       <About />
     </>
   );
