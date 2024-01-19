aebc12d621991075fe16c6a752f3994a5d16e4c4
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-01-19 15:14:43 -0300

    style: add default styles to ul, a and p element in CMSRichText component

--- src/components/RichText/Index.tsx
+++ src/components/RichText/Index.tsx
@@ -4,5 +4,20 @@ import { ComponentProps } from "react";
 type RichTextProps = ComponentProps<typeof CMSRichText>;
 
 export const RichText = ({ ...props }: RichTextProps) => {
-  return <CMSRichText {...props} />;
+  return (
+    <CMSRichText
+      {...props}
+      renderers={{
+        ul: ({ children }) => (
+          <ul className="flex list-inside list-disc flex-col gap-3 pl-2">
+            {children}
+          </ul>
+        ),
+        a: ({ children }) => (
+          <a className="cursor-pointer text-primary">{children}</a>
+        ),
+        p: ({ children }) => <p className="mb-4">{children}</p>,
+      }}
+    />
+  );
 };
